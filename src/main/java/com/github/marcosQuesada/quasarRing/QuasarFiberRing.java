1bb8bf68e3ca39cb6439da1ac4cff8bee0664985
marcosQuesada <marcos.quesadas@gmail.com>
2016-07-22 19:16:56 +0200

    base project

---
+++ src/main/java/com/github/marcosQuesada/quasarRing/QuasarFiberRing.java
@@ -0,0 +1,12 @@
+package com.github.marcosQuesada.quasarRing;
+
+
+public class QuasarFiberRing {
+    protected int iterations;
+    protected int nodesOnRing;
+
+    public QuasarFiberRing() {
+        //this.iterations = Integer.parseInt(System.getProperty("iterations"));
+        //this.nodesOnRing = Integer.parseInt(System.getProperty("nodesOnRing"));
+    }
+}
---
+++ src/test/java/com/github/marcosQuesada/quasarRing/QuasarFiberRingTest.java
@@ -0,0 +1,12 @@
+package com.github.marcosQuesada.quasarRing;
+
+import org.junit.Test;
+
+public class QuasarFiberRingTest {
+
+    @Test
+    public void testQuasarFiberRing() throws Exception {
+
+    }
+
+}
