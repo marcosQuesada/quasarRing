92c7d1e2d9c24eb139ad8da0594db3ce6ffecde8
marcosQuesada <marcos.quesadas@gmail.com>
2016-07-22 19:42:46 +0200

    first iteration

--- src/main/java/com/github/marcosQuesada/quasarRing/QuasarFiberRing.java
+++ src/main/java/com/github/marcosQuesada/quasarRing/QuasarFiberRing.java
@@ -1,12 +1,71 @@
 package com.github.marcosQuesada.quasarRing;
 
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.strands.Strand;
 
 public class QuasarFiberRing {
+
     protected int iterations;
     protected int nodesOnRing;
 
-    public QuasarFiberRing() {
-        //this.iterations = Integer.parseInt(System.getProperty("iterations"));
-        //this.nodesOnRing = Integer.parseInt(System.getProperty("nodesOnRing"));
+    public QuasarFiberRing(int iterations, int nodesOnRing) {
+        this.iterations = iterations;
+        this.nodesOnRing = nodesOnRing;
     }
+
+    class Node extends Fiber<Integer> {
+        protected Node nextNode;
+        protected int result;
+        protected boolean wait = true;
+
+        public Node(int id) {
+            super(String.format("%s-%d", Node.class.getSimpleName(), id));
+        }
+
+        @Override
+        public Integer run() throws SuspendExecution, InterruptedException {
+            do {
+                while (wait) {
+                    Strand.park();
+                }
+                wait = true;
+                nextNode.result = result - 1;
+                nextNode.wait = false;
+                Strand.unpark(nextNode);
+            } while (result > 0);
+            return result;
+        }
+    }
+
+
+    public int[] makeRing() throws Exception {
+        // Create fibers.
+        final Node[] nodes = new Node[iterations];
+        for (int i = 0; i < iterations; i++)
+            nodes[i] = new Node(i);
+
+        // Set next fiber pointers.
+        for (int i = 0; i < iterations; i++)
+            nodes[i].nextNode = nodes[(i + 1) % iterations];
+
+        // Start fibers.
+        for (final Node fiber : nodes) fiber.start();
+
+        // Initiate the ring.
+        final Node first = nodes[0];
+        first.result = iterations;
+        first.wait = false;
+        Strand.unpark(first);
+
+        // Wait for fibers to complete.
+        final int[] results = new int[iterations];
+        for (int i = 0; i < iterations; i++)
+            results[i] = nodes[i].get();
+
+
+        return results;
+    }
+
 }
+
--- src/test/java/com/github/marcosQuesada/quasarRing/QuasarFiberRingTest.java
+++ src/test/java/com/github/marcosQuesada/quasarRing/QuasarFiberRingTest.java
@@ -2,11 +2,34 @@ package com.github.marcosQuesada.quasarRing;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
+
 public class QuasarFiberRingTest {
 
     @Test
     public void testQuasarFiberRing() throws Exception {
+        Logger log = LoggerFactory.getLogger(QuasarFiberRingTest.class);
 
+        log.trace("Starting");
+        int workerCount = 100;
+        int ringSize = 100;
+        QuasarFiberRing qfr = new QuasarFiberRing(workerCount, ringSize);
+        int[] sequences = qfr.makeRing();
+
+        final int offset = workerCount - ringSize % workerCount;
+        for (int i = 0; i < workerCount; i++)
+            try {
+                assertEquals(
+                        "sequence returned by Worker#" + i,
+                        -((offset + i) % workerCount), sequences[i]);
+            } catch (AssertionError ae) {
+                log.trace("sequences[] = {}", Arrays.toString(sequences));
+                throw ae;
+            }
     }
 
 }
